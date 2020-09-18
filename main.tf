28863d91d5b692fe4fe98fcc5303f6b692a8c3d3
astazebay <abdullahtazebay@gmail.com>
2020-09-18 12:24:50 -0500

    Initial Commit

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ locals{
 module "create-sa-iam-policy" {
   //source = "github.com/InfraManAI/gcp-iam-modules.git//gcp-service-account-iam-policy"
   source = "../gcp-iam-modules/gcp-service-account-iam-policy"
-  policy_set = {
+  policy_set_policy = {
     policy_1 = {
       members_policy = [
         local.members_dic["m1"],
@@ -29,19 +29,22 @@ module "create-sa-iam-policy" {
         local.service_account_dic["sa1"],
        ]
       roles_policy = [
-        local.roles_dic["role1"]
+        local.roles_dic["role1"],
+        local.roles_dic["role3"]
       ],
       desc_policy = "Policy Test 1"
     }
 
-    binding_1 = {
+    policy_2 = {
       members_binding = [
         local.members_dic["m2"]]
       service_accounts_binding = [
         local.service_account_dic["sa2"],
        ]
       roles_binding = [
-        local.roles_dic["role2"]
+        local.roles_dic["role2"],
+        local.roles_dic["role1"],
+        local.roles_dic["role3"]
       ],
       desc_binding = "Policy Test 2"
       mode = "binding"
